5b8d2e3871b6279048890d272c92f741b74ba634
Colin Harris <col.w.harris@gmail.com>
2016-07-10 17:02:08 +0800

    Remove the user migration to avoid clashes with the host app migrations

--- priv/repo/migrations/20160704132459_add_user.exs
+++ priv/repo/migrations/20160704132459_add_user.exs
@@ -2,11 +2,12 @@ defmodule Oauthenator.Repo.Migrations.AddUser do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
-      add :email, :string
-      add :password, :string
-
-      timestamps
-    end
+    # TODO: This should be done by the host app
+    # create table(:users) do
+    #   add :email, :string
+    #   add :password, :string
+    #
+    #   timestamps
+    # end
   end
 end
